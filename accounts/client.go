97ba985940c928e313f27b15469f4f2b6e8f15f0
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2021-10-16 13:48:22 +0200

    Moves the deletion capability to the client file

--- accounts/client.go
+++ accounts/client.go
@@ -76,3 +76,16 @@ func (client *Client) FetchResource(accountID string) (*AccountData, error) {
 
 	return responsePayload.Data, nil
 }
+
+func (client *Client) DeleteResource(accountID string, version int) error {
+	if err := validateAccountIDFormat(accountID); err != nil {
+		return fmt.Errorf("%w; unable to delete resource", err)
+	}
+
+	resourcePath := fmt.Sprintf("%s/%s?version=%d", basePath, accountID, version)
+	if err := client.http.Delete(resourcePath); err != nil {
+		return fmt.Errorf("%w; unable to delete resource", err)
+	}
+
+	return nil
+}
