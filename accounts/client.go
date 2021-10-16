8ea82479bc166d4124e91400eca74da17812a7ee
Renato Rodrigues de Araujo <renato.r.araujo@gmail.com>
2021-10-16 13:46:46 +0200

    Moves the fetch capability to the client file

--- accounts/client.go
+++ accounts/client.go
@@ -57,3 +57,22 @@ func (client *Client) CreateResource(accountData *AccountData) (*AccountData, er
 
 	return responsePayload.Data, nil
 }
+
+func (client *Client) FetchResource(accountID string) (*AccountData, error) {
+	if err := validateAccountIDFormat(accountID); err != nil {
+		return nil, fmt.Errorf("%w; unable to fetch resource", err)
+	}
+
+	resourcePath := fmt.Sprintf("%s/%s", basePath, accountID)
+	apiResponse, err := client.http.Get(resourcePath)
+	if err != nil {
+		return nil, fmt.Errorf("%w; unable to fetch resource", err)
+	}
+
+	responsePayload := &payload{}
+	if err = json.Unmarshal(apiResponse, responsePayload); err != nil {
+		return nil, errors.New("failed to unmarshal response data")
+	}
+
+	return responsePayload.Data, nil
+}
--- accounts/client_test.go
+++ accounts/client_test.go
@@ -2,6 +2,7 @@ package accounts
 
 import (
 	"errors"
+	"io/ioutil"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -102,4 +103,78 @@ func TestCreateAccount(t *testing.T) {
 			mock.AssertExpectationsForObjects(t, httpUtilsMock)
 		})
 	}
+}
+
+func TestFetchAccount(t *testing.T) {
+	tests := []struct {
+		name           string
+		AccountID      string
+		httpUtilsSetup func(utils *mockHttpUtils)
+		wantErr        bool
+	}{
+		{
+			name:      "Failed to fetch an account due a not found account id",
+			AccountID: "00000000-0000-0000-0000-000000000000",
+			httpUtilsSetup: func(client *mockHttpUtils) {
+				client.On("Get", mock.Anything).Return(
+					nil,
+					errors.New("not found"),
+				)
+			},
+			wantErr: true,
+		},
+		{
+			name:      "Failed to fetch data due a missing data in response format",
+			AccountID: "00000000-0000-0000-0000-000000000000",
+			httpUtilsSetup: func(client *mockHttpUtils) {
+				client.On("Get", mock.Anything).Return(
+					[]byte("invalid json"),
+					errors.New("unable to unmarshal invalid json"),
+				)
+			},
+			wantErr: true,
+		},
+		{
+			name:      "Successfully fetch an account",
+			AccountID: "00000000-0000-0000-0000-000000000000",
+			httpUtilsSetup: func(client *mockHttpUtils) {
+				client.On("Get", mock.Anything).Return(
+					loadTestFile("./testdata/fetch_response.json"),
+					nil,
+				)
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+
+			httpUtilsMock := &mockHttpUtils{}
+			if tt.httpUtilsSetup != nil {
+				tt.httpUtilsSetup(httpUtilsMock)
+			}
+
+			accountsClient := NewClient(httpUtilsMock)
+			accountData, err := accountsClient.FetchResource(tt.AccountID)
+			if tt.wantErr {
+				require.Error(t, err)
+			} else {
+				require.NoError(t, err)
+			}
+
+			if accountData != nil {
+				assert.IsType(t, &AccountData{}, accountData)
+			}
+		})
+	}
+}
+
+func loadTestFile(file string) []byte {
+	raw, err := ioutil.ReadFile(file)
+	if err != nil {
+		panic("failed to load the test data file")
+	}
+
+	return raw
 }
